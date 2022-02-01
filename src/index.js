4e2b01a9be8f79dee093b1e4a19c3d4cf507404f
Pahuni Telang <telangpahuni@gmail.com>
2022-02-01 13:19:03 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
 import store from './redux/store';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
